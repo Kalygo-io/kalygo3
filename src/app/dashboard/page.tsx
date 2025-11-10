030e888c7c1d4e6154998ec603e707ec24f53aac
Taddy <thaddadavis@gmail.com>
2025-11-10 10:00:46 -0500

    aiSchool Agent

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,5 +4,5 @@ import { protectedPageGuard } from "@/components/shared/utils/validate-token";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-  redirect("/dashboard/tokenizers");
+  redirect("/dashboard/agentic-rag");
 }
